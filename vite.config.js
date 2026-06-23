701d6718c8b758dc54b0c676ff7de0780cc81102
lamB2306626 <lamb2306626@student.ctu.edu.vn>
2026-06-23 12:04:17 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,15 @@ export default defineConfig({
     },
   },
 
-  server: {
-    port: 3001,
-  },
+    server: {
+      port: 3001,
+  proxy: {
+    "/api": {
+    target: "http://localhost:3000/",
+    changeOrigin: true,
+    },
+  }
+    },
 })
 
 
