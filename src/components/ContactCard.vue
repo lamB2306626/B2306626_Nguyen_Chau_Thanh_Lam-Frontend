f97b2a2d49558a3fe3f09bb9db2596bfa0c9b88e
lamB2306626 <lamb2306626@student.ctu.edu.vn>
2026-06-25 18:43:49 +0700

    Tao trang cap nhat lien he

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -1,32 +1,37 @@
 <script>
 export default {
-props: {
-contact: { type: Object, required: true },
-},
+  props: {
+    contact: { type: Object, required: true },
+  },
 };
 </script>
 <template>
-<div>
-<div class="p-1">
-<strong>Tên:</strong>
-{{ contact.name }}
-</div>
-<div class="p-1">
-<strong>E-mail:</strong>
-{{ contact.email }}
-</div>
-<div class="p-1">
-<strong>Địa chỉ:</strong>
-{{ contact.address }}
-</div>
-<div class="p-1">
-<strong>Điện thoại:</strong>
-{{ contact.phone }}
-</div>
-<div class="p-1">
-<strong>Liên hệ yêu thích:&nbsp;</strong>
-<i v-if="contact.favorite" class="fas fa-check"></i>
-<i v-else class="fas fa-times"></i>
-</div>
-</div>
+  <div>
+    <div class="p-1">
+      <strong>Tên:</strong>
+      {{ contact.name }}
+    </div>
+    <div class="p-1">
+      <strong>E-mail:</strong>
+      {{ contact.email }}
+    </div>
+    <div class="p-1">
+      <strong>Địa chỉ:</strong>
+      {{ contact.address }}
+    </div>
+    <div class="p-1">
+      <strong>Điện thoại:</strong>
+      {{ contact.phone }}
+    </div>
+    <div class="p-1">
+      <strong>Yêu thích:</strong>
+      {{ contact.favorite }}
+    </div>
+
+    <div class="p-1">
+      <strong>Liên hệ yêu thích:&nbsp;</strong>
+      <i v-if="contact.favorite" class="fas fa-dot-circle text-primary"></i>
+      <i v-else class="far fa-circle text-secondary"></i>
+    </div>
+  </div>
 </template>
